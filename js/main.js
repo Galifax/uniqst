2c4a623d5240586ab817a40c59817e11523a819f
LtFrager <sadesade2011@gmail.com>
2017-06-07 13:45:39 +0300

    Header-3

--- js/main.js
+++ js/main.js
@@ -36,3 +36,18 @@ $(window).on('resize', function(){
 		resizing = true;
 	}
 });
+
+// LEO SCTIPT
+//
+// $(document).ready(function(){
+//     $("#menu").on("click","a", function (event) {
+//         //отменяем стандартную обработку нажатия по ссылке
+//         event.preventDefault();
+//         //забираем идентификатор бока с атрибута href
+//         var id  = $(this).attr('href'),
+//             //узнаем высоту от начала страницы до блока на который ссылается якорь
+//             top = $(id).offset().top;
+//         //анимируем переход на расстояние - top за 1500 мс
+//         $('body,html').animate({scrollTop: top}, 1500);
+//     });
+// });
