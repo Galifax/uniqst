04e317b48357dc3944df7c13f1559b894b24c10a
LtFrager <sadesade2011@gmail.com>
2017-06-07 18:28:59 +0300

    Alpha_v.1

--- js/main.js
+++ js/main.js
@@ -48,7 +48,7 @@ $(document).ready(function(){
             //узнаем высоту от начала страницы до блока на который ссылается якорь
             top = $(id).offset().top;
         //анимируем переход на расстояние - top за 1500 мс
-        $('body,html').animate({scrollTop: top}, 1500);
+        $('body,html').animate({scrollTop: top}, 3500);
     });
 });
 
