13e2e24f88f8b331534f75ec67ff6884809cd0f9
LtFrager <sadesade2011@gmail.com>
2017-06-12 15:14:11 +0300

    VLAD_ZAKONCHI

--- js/main.js
+++ js/main.js
@@ -52,18 +52,7 @@ $(window).on('resize', function(){
 
 // LEO SCTIPT
 
-$(document).ready(function() {
-    $("#menu").on("click", "button", function (event) {
-        //отменяем стандартную обработку нажатия по ссылке
-        event.preventDefault();
-        //забираем идентификатор бока с атрибута href
-        var id = $(this).attr('href'),
-            //узнаем высоту от начала страницы до блока на который ссылается якорь
-            top = $(id).offset().top;
-        //анимируем переход на расстояние - top за 1500 мс
-        $('body,html').animate({scrollTop: top}, 1500);
-    });
-});
+
 
 //Calculator script
 
@@ -80,3 +69,4 @@ $(document).ready(function() {
 //         return false;
 //     });
 // });
+// portfilio
